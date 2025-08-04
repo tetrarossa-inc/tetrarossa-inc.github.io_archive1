b422e2816dd66b105210b8f40f82d9e043a776c1
honami2oyama <honami2oyama@gmail.com>
2025-08-05 06:15:35 +0800

    Website update v1.3

--- finale/finale.js
+++ finale/finale.js
@@ -1,6 +1,22 @@
-const follower = document.getElementById('follower');
+// Array of audio file paths
+const audioFiles = [
+	'finale/jevil_anything.mp3',
+	'finale/jevil_laugh1.mp3',
+	'finale/jevil_laugh2.mp3',
+	'finale/jevil_laugh3.mp3',
+	'finale/jevil_laugh4.mp3',
+	'finale/jevil_oh.mp3'
+];
 
-document.addEventListener('mousemove', (e) => {
-	follower.style.left = `${e.clientX}px`;
-	follower.style.top = `${e.clientY}px`;
-});
+// Reference to the <audio> element using its ID
+const audioPlayer = document.getElementById('myAudio');
+
+// Random index within the range of the audioFiles array's length
+function playRandomAudio() {
+	const randomIndex = Math.floor(Math.random() * audioFiles.length);
+	audioPlayer.src = audioFiles[randomIndex];
+	audioPlayer.play();
+}
+
+// Random sound every 5 seconds
+setInterval(playRandomAudio, 5000);
